71078af79c014237f79c92d81e9004c6bdd99db7
cx.li <chunxialims@163.com>
2020-12-24 19:27:01 +0800

    netty grouping chat

---
+++ src/main/java/com/atguigu/netty/groupchat/GroupChatServer.java
@@ -0,0 +1,19 @@
+package com.atguigu.netty.groupchat;
+
+/**
+ * @Author LiChunxia
+ * @Date 2020/12/24  12:46
+ */
+public class GroupChatServer {
+
+    private int port;
+
+    public GroupChatServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+
+    }
+
+}
